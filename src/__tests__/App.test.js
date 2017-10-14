7bccfc075b04473b151dff78cbdcbdbe3ed1e183
felipefreitag <ffvargas@gmail.com>
2017-10-14 16:30:49 -0300

    Starts a new test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+//import Enzyme, { shallow } from 'enzyme'
+//import Adapter from 'enzyme-adapter-react-16'
+import { employeeResults, companyResults } from '../functions'
 import App from '../App'
+
+jest.mock('../functions', () => {
+  employeeResults: jest.fn(() => ({
+    name: 'employee',
+    yearly: 1000.0,
+    fgts: 20.0,
+  }))
+
+  companyResults: jest.fn(() => ({
+    name: 'employee',
+    yearly: 2000.0,
+  }))
+})
+
+//Enzyme.configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -14,3 +32,12 @@ it('renders correctly', () => {
     ).toJSON()
     expect(tree).toMatchSnapshot()
 })
+
+// describe('calculate', () => {
+//   it('stores the results in the state', () => {
+//     const wrapper = shallow(<App />)
+//     wrapper.instance().calculate()
+//     expect(employeeResults).toHaveBeenCalled()
+//     expect(companyResults).toHaveBeenCalled()
+//   })
+// })
